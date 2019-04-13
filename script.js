21ac1238b5a1907db0aaeda8a649afc27612ed5e
utkarsh patel <hirenkumar@me.com>
2019-04-13 11:45:51 +1000

    primary time stamp getting setting

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 //alert();
+let textBox ;
 
 let btnApply = document.getElementById("btnApply");
 btnApply.addEventListener("click",evokeNotification);
 
 function evokeNotification(){
+    alert(new Date());
+    console.log(new Date().getTime());
+    let nowTime = new Date().getTime();
+    
+    setTimeout(myNotefication,2000);
+}
+
+function getMessage (){
+    let tt = document.getElementById("inputBox");
+    textBox = tt.value;
+    return textBox;
+}
+
+function myNotefication(){
     // Gets permission for notification and creates notification
     Notification.requestPermission().then(function (result) {
         console.log(result);
         //alert("Got Notification Permission");
-        var notification = new Notification("Hi there! My First Sample Notification");
+        var notification = new Notification(getMessage());
         if (Notification.permission === "granted"){
             alert("yes");
         }
@@ -16,3 +31,4 @@ function evokeNotification(){
         alert("See "+ err);
     });
 }
+
