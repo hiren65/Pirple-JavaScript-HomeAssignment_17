8b623d6a5b4c5472b01f2a7bd74871a818f77d91
utkarsh patel <hirenkumar@me.com>
2019-04-13 10:51:01 +1000

    First notification created and button event click attached to notification creates

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert();
+//alert();
+
+let btnApply = document.getElementById("btnApply");
+btnApply.addEventListener("click",evokeNotification);
+
+function evokeNotification(){
+    // Gets permission for notification and creates notification
+    Notification.requestPermission().then(function (result) {
+        console.log(result);
+        //alert("Got Notification Permission");
+        var notification = new Notification("Hi there! My First Sample Notification");
+        if (Notification.permission === "granted"){
+            alert("yes");
+        }
+    }).catch(function (err) {
+        alert("See "+ err);
+    });
+}
